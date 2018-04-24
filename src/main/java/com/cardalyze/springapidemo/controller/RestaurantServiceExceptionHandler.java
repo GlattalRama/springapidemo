d02eb973162a96c13af8d507eef5182629298c13
Manish Verma <mverma@cardalyze.com>
2018-04-24 15:48:15 -0400

    Adding exception handler and controller advice

---
+++ src/main/java/com/cardalyze/springapidemo/controller/RestaurantServiceExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.cardalyze.springapidemo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class RestaurantServiceExceptionHandler {
+	
+	public ResponseEntity<ErrorResponse> handleServiceException(RestaurantServiceException e) throws Exception
+	{
+		ErrorResponse eR = new ErrorResponse();
+		eR.setCode(HttpStatus.BAD_REQUEST.value());
+		eR.setMessage(e.getMessage());
+		return new ResponseEntity<ErrorResponse>(eR, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(RuntimeException.class) public ResponseEntity<ErrorResponse> handleGeneralException(RuntimeException e) throws Exception
+	{
+		ErrorResponse eR = new ErrorResponse();
+		eR.setCode(HttpStatus.SERVICE_UNAVAILABLE.value());
+		eR.setMessage(e.getMessage());
+		return new ResponseEntity<ErrorResponse>(eR, HttpStatus.SERVICE_UNAVAILABLE);
+	}
+
+}
